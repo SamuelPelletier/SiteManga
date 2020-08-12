e9b748e0c8be1a9d077a52bd4adccca5813849fe
PELLETIER Samuel <pellesam.info@gmail.com>
2020-08-12 16:15:39 +0200

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SiteManga.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
